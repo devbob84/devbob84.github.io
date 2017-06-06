c6cf8f23fec18f209f9e127db51650d7050a53d2
rjrutman <rjrutman@gmail.com>
2017-06-05 21:41:04 -0700

    uppload website

---
+++ javaScript/myScript.js
@@ -0,0 +1,17 @@
+$(function() {
+    
+ "use strict";
+    
+    //Activte Scrollspy
+    
+    $('body').srrollspy({
+        target: 'header .navbar'
+    });
+    
+       $('.carousel').carousel({
+  interval: false
+});
+    
+});
+
+
